73e1046e03ee726c2cb7793e2427e0b697f5b6c4
Paige <paige.vogenthaler@gmail.com>
2017-02-15 20:43:34 -0800

    dynamically change favicon testing

--- public/app.js
+++ public/app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
     link.type = 'image/x-icon';
     link.rel = 'shortcut icon';
-    link.href = 'myfavicon.ico';
+    link.href = 'http://www.stackoverflow.com/favicon.ico';
     document.getElementsByTagName('head')[0].appendChild(link);
   }());
 
